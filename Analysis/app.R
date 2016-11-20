01f2f50c1bb9d1ef6d0fe55c2d859cb7ce1314ce
dscmith15 <Smithdc15@gmail.com>
2016-11-20 15:54:01 -0600

    Update app.R

--- Analysis/app.R
+++ Analysis/app.R
@@ -1,3 +1,9 @@
 #A place to take the results from our pilot study and plot them for committee members and others that are interested
 
 library(shiny)
+
+ui <- fluidPage("Pilot Results")
+
+server <- function(input, output){}
+
+shinyApp(ui = ui, server = server)
