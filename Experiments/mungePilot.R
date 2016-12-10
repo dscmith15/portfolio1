93931b0ecf4a34d96e9812e342a5c10d22935269
dscmith15 <Smithdc15@gmail.com>
2016-12-10 14:50:38 -0600

    Update mungePilot.R

--- Experiments/mungePilot.R
+++ Experiments/mungePilot.R
@@ -64,12 +64,12 @@ write.table(results, "results.csv", col.names = FALSE, row.names = FALSE, sep = 
 pfinal["odd"] <- revalue(pfinal$ptemp.odd, c("100%"=1,"80%"=4,"40%"=1.5,"25%"=3,"10%"=9))
 pfinal["delay"] <- revalue(pfinal$ptemp.delay, c("immediately"=1,"in 1 month"=4,"in 6 months"=1.5,"in 2 years"=3,"in 5 years"=9))
 
-#nlmodel <- nls(ptemp.subvalue~(40000/((1+odd)*((1+delay)^.5))), data = pfinal, start=list(odd = 1.4, delay = 5))
+nlmodel <- nls(ptemp.subvalue~(40000/((1+odd)*((1+delay)^.5))), data = pfinal, start=list(odd = 1.4, delay = 5))
 #print(nlmodel)
 
-setwd("/var/www/html/")
-HTML(pfinal, file = "pilotdata.html", Border = 1, innerBorder = 0,classfirstline = "firstline",
-  classfirstcolumn = "firstcolumn", classcellinside = "cellinside", append = TRUE)
+#setwd("/var/www/html/")
+#HTML(pfinal, file = "pilotdata.html", Border = 1, innerBorder = 0,classfirstline = "firstline",
+#  classfirstcolumn = "firstcolumn", classcellinside = "cellinside", append = TRUE)
 
 setwd("/var/www/html/result/")
 # to get the data for the histogram
