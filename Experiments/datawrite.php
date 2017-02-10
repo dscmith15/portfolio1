b1da8ab3e8a7d31d5c84b8ebb1bd06fefb459487
dscmith15 <Smithdc15@gmail.com>
2017-02-10 11:57:57 -0600

    trying to fix file size limits

--- Experiments/datawrite.php
+++ Experiments/datawrite.php
@@ -2,6 +2,6 @@
 // this writes data to local server storage on the pi
 $filename = "dataP1/".$_POST['filename'];
 $data = $_POST['filedata'];
-//write the file to the damn disk
+//write the file to the disk
 file_put_contents($filename, $data);
 ?>
