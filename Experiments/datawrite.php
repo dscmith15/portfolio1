c505ab8d82215f68799098ca816e3938bb182b6f
dscmith15 <Smithdc15@gmail.com>
2017-02-22 15:56:50 -0600

    prepping new phase

--- Experiments/datawrite.php
+++ Experiments/datawrite.php
@@ -1,6 +1,6 @@
 <?php
 // this writes data to local server storage on the pi
-$filename = "dataP2/".$_POST['filename'];
+$filename = "dataP3/".$_POST['filename'];
 $data = $_POST['filedata'];
 //write the file to the disk
 file_put_contents($filename, $data);
